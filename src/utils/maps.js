aa869ba8f8ff7d7b35f54b8e27d7d947750da45c
marianosim <mariano.simantob@gmail.com>
2023-06-12 17:21:28 -0300

    Changed Maps api key.

--- src/utils/maps.js
+++ src/utils/maps.js
@@ -1,4 +1,4 @@
-export const MAPS_API_KEY = 'AIzaSyD7uin4bvQZzX8GRpF_Hd-UUzuEoFGfqRM';
+export const MAPS_API_KEY = '';
 
 export const URL_MAPS = (lat, lng, zoom = 14) =>
   `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap
